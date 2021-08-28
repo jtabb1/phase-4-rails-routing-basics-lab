adc32e7e735708de99699f3b5bece4af42ca39e9
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-08-28 19:48:12 -0400

    Finish main assignment with the help of the official solution.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,10 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+
+  def grades 
+    students = Student.order(grade: :desc)
+    render json: students
+  end
   
 end
